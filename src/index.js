61925b5ec40ea0a9106dfd21284378c9f5b20b13
admin <ale.krecisz@gmail.com>
2020-04-27 16:32:44 +0200

    add analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
 import Scrollbar from 'smooth-scrollbar';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-164675990-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 Scrollbar.init(document.querySelector('#root'));
 
